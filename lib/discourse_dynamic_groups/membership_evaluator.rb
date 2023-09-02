bdaff124795f328574c9b52a61b61fec9dfeeeb3
Richard <richard@communiteq.com>
2023-09-02 08:48:20 +0000

    Fix typo

--- lib/discourse_dynamic_groups/membership_evaluator.rb
+++ lib/discourse_dynamic_groups/membership_evaluator.rb
@@ -2,18 +2,6 @@
 
 module DiscourseDynamicGroups
   class MembershipEvaluator
-    def determine_group_membership(user, group)
-      # get rules for group
-      # run rule engine
-      # add or remove user from group if necessary
-    end
-
-    def evaluate_dependent_groups(user, group)
-      # get the dependent groups for this group
-      # loop through them
-      # call determine_group_membership
-    end
-
     def evaluate_group(group)
       engine = ::DiscourseDynamicGroups::RuleEngine.new
       user_ids = engine.magical_tree(group.dynamic_rule)
@@ -23,7 +11,7 @@ module DiscourseDynamicGroups
       User.where(id: to_be_removed).each do |user|
         group.remove(user)
       end
-      User.where(id: to_be_added).each do |user}
+      User.where(id: to_be_added).each do |user|
         group.add(user)
       end
     end
