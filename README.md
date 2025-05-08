f08d27544b519d565e6ddc11ba445b0bdb5eb53f
Richard <richard@communiteq.com>
2025-05-08 16:18:59 +0000

    Add meta topic id

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dynamic Groups plugin
+
+For more information, please see https://meta.discourse.org/t/discourse-dynamic-groups/365342
--- plugin.rb
+++ plugin.rb
@@ -3,6 +3,7 @@
 # name: discourse-dynamic-groups
 # about: Automatically populate groups
 # version: 1.3.1
+# meta_topic_id: 365342
 # authors: Communiteq
 # url: https://github.com/communiteq/discourse-dynamic-groups
 
